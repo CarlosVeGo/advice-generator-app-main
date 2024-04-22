9d0151bd5bee2cea88ec2ad44e7c63f9fed855c4
CarlosVeGo <carlos.velezg2011@gmail.com>
2024-04-22 15:57:36 -0500

    fetch and catch completed

--- index.js
+++ index.js
@@ -2,24 +2,29 @@ const container = document.querySelector(".container");
 const adviceNumber = document.querySelector(".advice-number");
 const adviceContent = document.querySelector("p");
 const button = document.querySelector("button");
-let apiLink = `https://api.adviceslip.com/advice`;
+const apiLink = `https://api.adviceslip.com/advice`;
 let numero;
 let consejo;
 
 button.addEventListener("click",() =>{
     
-    
-    
+    adviceContent.textContent = `Loading...`;
+
     fetch(apiLink).then((data)=>data.json().then((res)=>{
         
         numero = res.slip.id;
         consejo = res.slip.advice;
         
-        console.log(res)
-        console.log(numero)
-        console.log(consejo)
+        console.log(res);
+        console.log(numero);
+        console.log(consejo);
         
         adviceNumber.textContent= `advice #${numero}`
-        adviceContent.textContent=`${consejo}`
-    }))
+        adviceContent.textContent=`${consejo}`;
+
+        
+    })).catch((err)=>
+        {
+            return adviceContent.textContent = `try again later`;
+    })
 })
